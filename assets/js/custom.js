ad70ee3016ed0e106425e313d80582d050232eee
Rizwaan <rizraak@hotmail.com>
2020-05-29 04:05:30 -0400

    New Site

---
+++ assets/js/custom.js
@@ -0,0 +1,170 @@
+(function ($) {
+	
+	"use strict";
+
+	// Window Resize Mobile Menu Fix
+	mobileNav();
+
+
+	// Scroll animation init
+	window.sr = new scrollReveal();
+	
+
+	// Menu Dropdown Toggle
+	if($('.menu-trigger').length){
+		$(".menu-trigger").on('click', function() {	
+			$(this).toggleClass('active');
+			$('.header-area .nav').slideToggle(200);
+		});
+	}
+
+
+	// Menu elevator animation
+	$('a[href*=\\#]:not([href=\\#])').on('click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				var width = $(window).width();
+				if(width < 991) {
+					$('.menu-trigger').removeClass('active');
+					$('.header-area .nav').slideUp(200);	
+				}				
+				$('html,body').animate({
+					scrollTop: (target.offset().top) - 130
+				}, 700);
+				return false;
+			}
+		}
+	});
+
+	$(document).ready(function () {
+	    $(document).on("scroll", onScroll);
+	    
+	    //smoothscroll
+	    $('a[href^="#"]').on('click', function (e) {
+	        e.preventDefault();
+	        $(document).off("scroll");
+	        
+	        $('a').each(function () {
+	            $(this).removeClass('active');
+	        })
+	        $(this).addClass('active');
+	      
+	        var target = this.hash,
+	        menu = target;
+	       	var target = $(this.hash);
+	        $('html, body').stop().animate({
+	            scrollTop: (target.offset().top) - 130
+	        }, 500, 'swing', function () {
+	            $(document).on("scroll", onScroll);
+	        });
+	    });
+	});
+
+	function onScroll(event){
+	    var scrollPos = $(document).scrollTop();
+	    $('.nav a').each(function () {
+	        var currLink = $(this);
+			var refElement = $(currLink.attr("href"));
+			var dtt = refElement.find("h2").offset().top - $(window).scrollTop();
+			var dt2 = refElement.find("h2").css("padding-top");
+			dtt = parseInt(dt2) + dtt;
+			var height = $(window).height();
+			if (height >= dtt + 120) {
+				$('a').removeClass("active");
+				currLink.addClass("active");
+	        }
+	        else{
+	            currLink.removeClass("active");
+	        }
+	    });
+	}
+
+
+	// Home seperator
+	if($('.home-seperator').length) {
+		$('.home-seperator .left-item, .home-seperator .right-item').imgfix();
+	}
+
+
+	// Home number counterup
+	if($('.count-item').length){
+		$('.count-item strong').counterUp({
+			delay: 10,
+			time: 1000
+		});
+	}
+
+
+	// Page loading animation
+	$(window).on('load', function() {
+		if($('.cover').length){
+			$('.cover').parallax({
+				imageSrc: $('.cover').data('image'),
+				zIndex: '1'
+			});
+		}
+
+		$("#preloader").animate({
+			'opacity': '0'
+		}, 600, function(){
+			setTimeout(function(){
+				$("#preloader").css("visibility", "hidden").fadeOut();
+			}, 300);
+		});
+	});
+
+
+	// Window Resize Mobile Menu Fix
+	$(window).on('resize', function() {
+		mobileNav();
+	});
+
+
+	// Window Resize Mobile Menu Fix
+	function mobileNav() {
+		var width = $(window).width();
+		$('.submenu').on('click', function() {
+			if(width < 992) {
+				$('.submenu ul').removeClass('active');
+				$(this).find('ul').toggleClass('active');
+			}
+		});
+	}
+
+
+})(window.jQuery);
+
+// Form Submit
+
+$(function(){
+    $(".ajaxForm").submit(function(e){
+		var error = true;
+		console.log("Fail");
+        e.preventDefault();
+        var href = $(this).attr("action");
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+			url: href,
+			async: false,
+            data: $(this).serialize(),
+            success: function(response){
+                if(response.status == "success"){
+					$("#emailFail").hide();
+					$("#emailSuc").show();
+					error = false;
+                }
+            }
+		});
+		if(error == true){
+			$("#emailSuc").hide();
+			$("#emailFail").show();
+		}else{
+			$("#email").val("");
+			$("#message").val("");
+			$("#name").val("");
+		}
+    });
+});
